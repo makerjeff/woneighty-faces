e01908b218acc2679799b70063995faf0c8a7405
Jeff Wu <makerjeffwu@gmail.com>
2016-04-12 10:49:15 -0700

    latest and greatest from laptop, continuing to build new server

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 /**
+ * 180 Faces - Hardware Interface
  * Created by jefferson.wu on 4/11/16.
  */
+
+// SOCKET.IO BOILERPLATE
+var express = require('express');
+var http = require('http');
+var app = express();
+var server = http.createServer(app);
+var io = require('socket.io')(server);
+// SOCKET.IO BOILERPLATE - END
+
+// serve static pages
+app.use(express.static(__dirname + '/public'));
+
+
+
