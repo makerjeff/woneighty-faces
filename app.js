763dbad3ba5f9aed4a955d0bf6873cdf5a74cc95
Jefferson Wu <jefferson.wu@180la.com>
2016-04-13 13:52:37 -0700

    continue working on the viewer

--- app.js
+++ app.js
@@ -26,6 +26,11 @@ app.get('/debug', function(request, response){
     response.sendFile(__dirname + '/public/debug.html');
 });
 
+app.get('/viewer', function(request, response){
+    response.type('text/html');
+    response.sendFile(__dirname + '/public/debug-viewer.html');
+});
+
 // SOCKET.IO
 io.on('connection', function(socket){
     allClients.push(socket);
@@ -39,8 +44,8 @@ io.on('connection', function(socket){
     // ==== SOCKET EVENTS ====
 
     //on receiving of data
-    socket.on('data', function(message){
-        var stringObject = message;
+    socket.on('data', function(data){
+        var stringObject = data;
         var dataObject = JSON.parse(stringObject);
         console.log(socket.client.id.toString().green + ': ' + dataObject.head + ', ' + dataObject.body);
     });
@@ -74,5 +79,9 @@ function initServer(port){
 function numberOfClients(allClientsArr){
     console.log('Total clients: ' + allClientsArr.length);
     return allClientsArr.length;
+}
+
+function updateViewer(dataObject){
+
 }
 
