0eda1ae884fd5fdbb3b7bdd39555e0790a9250fd
Jeff Wu <makerjeffwu@gmail.com>
2016-04-19 13:42:18 -0700

    latest and greatest, deciding on a module / object format, but did it inside checksum.js

--- _examples-checksum/checksum.js
+++ _examples-checksum/checksum.js
@@ -10,10 +10,50 @@ var md5message = checksum(message);
 console.log(message);
 console.log(md5message);
 
-checksum.file('checksum.js', function(error, sum){
-    if(error){
-        console.log(Error('something horrible happened!'));
-    } else {
-        console.log(sum)
-    }
-});
+// checksum.file('checksum.js', function(error, sum){
+//     if(error){
+//         console.log(Error('something horrible happened!'));
+//     } else {
+//         console.log(sum)
+//     }
+// });
+
+var checker = new JeffChecker();
+
+for(var i = 0; i < 10; i++){
+    checker.moreFun();
+}
+
+
+
+/* FUNCTIONS / OBJECTS */
+
+
+//type 1 object
+function JeffChecker(str){
+    this.funFactor = 0;
+    this.debugMessage = str;
+}
+JeffChecker.prototype.moreFun = function(val){
+    this.funFactor += val;
+    console.log(this.funFactor);
+};
+JeffChecker.prototype.lessFun = function(val){
+    this.funFactor -= val;
+    console.log(this.funFactor);
+};
+
+//type 2 object
+function JeffChecker2(str){
+    this.funFactor = 0;
+
+    //encapsulated moreFun
+    this.moreFun = function(val){
+        this.funFactor += val;
+    };
+
+    //encapsulated lessFun
+    this.lessFun = function(val){
+        this.funFactor -= val;
+    };
+}
