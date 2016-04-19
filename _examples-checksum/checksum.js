c97a9212535b46a7af619fc75320d3acaf711553
Jeff Wu <makerjeffwu@gmail.com>
2016-04-18 23:42:33 -0700

    working example of checksum

--- _examples-checksum/checksum.js
+++ _examples-checksum/checksum.js
@@ -5,5 +5,15 @@
 var checksum = require('checksum');
 
 var message = process.argv[2];
+var md5message = checksum(message);
 
 console.log(message);
+console.log(md5message);
+
+checksum.file('checksum.js', function(error, sum){
+    if(error){
+        console.log(Error('something horrible happened!'));
+    } else {
+        console.log(sum)
+    }
+});
