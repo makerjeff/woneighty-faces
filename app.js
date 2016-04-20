c7b8a9bdd67ccdd5eb14673af1f45844e833bbb6
Jefferson Wu <jefferson.wu@180la.com>
2016-04-20 16:30:52 -0700

    adding env test

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
  * Created by jefferson.wu on 4/11/16.
  * Namespaced + Jade
  *
- * TODO: create a new random object with local RGBholder values on 'connect', Destroy on 'disconnect'
  * TODO: environmental variables.  Use them.
  */
 
